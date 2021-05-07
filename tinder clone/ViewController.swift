acb02ce939f783f312fe798d8dbd26a905737e6a
Rytel <gobalek1@gmail.com>
2021-05-07 15:35:16 +0200

    Initial Commit

---
+++ tinder clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tinder clone
+//
+//  Created by Rafał Rytel on 07/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
