7c729c6e10c694874f7559f774bf1bab91530a60
Rytel <gobalek1@gmail.com>
2021-05-08 11:40:21 +0200

    first page UI

--- tinder clone/ViewController.swift
+++ tinder clone/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var signInFacebookButton: UIButton!
+    @IBOutlet weak var signInGoogleButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
