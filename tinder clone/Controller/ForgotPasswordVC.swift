efbfbc5b03bebc5f4bf56e6d321ab6c77ae89f99
Rytel <gobalek1@gmail.com>
2021-05-09 13:53:35 +0200

    forgot Password

---
+++ tinder clone/Controller/ForgotPasswordVC.swift
@@ -0,0 +1,31 @@
+//
+//  ForgotPasswordVC.swift
+//  tinder clone
+//
+//  Created by Rafał Rytel on 09/05/2021.
+//
+
+import UIKit
+
+class ForgotPasswordVC: UIViewController {
+
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var resetPasswordButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
